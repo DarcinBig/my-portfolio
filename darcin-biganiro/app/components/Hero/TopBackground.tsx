8a396977687a2d06ca23013af60598e067efc28d
DarcinBig <biganirodarcin@gmail.com>
2025-07-22 23:24:27 +0200

    Update TopBackground component with new icon colors

--- darcin-biganiro/app/components/Hero/TopBackground.tsx
+++ darcin-biganiro/app/components/Hero/TopBackground.tsx
@@ -6,22 +6,22 @@ const TopBackground = () => {
     <>
       <div className='absolute w-full max-w-[360px] md:max-w-screen-lg h-[285px] md:h-[656px] top-[188px] md:top-11 left-1/2 -translate-x-1/2 md:overflow-x-hidden z-10'>
         <div className='w-full h-full relative'>
-          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff] bg-opacity-[12%] absolute top-0 left-[171px] md:top-28 md:left-72'>
+          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff52] bg-opacity-[12%] absolute top-0 left-[171px] md:top-28 md:left-72'>
             <Image src='/rocket_icon.svg' alt='rocket icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
-          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff] bg-opacity-[12%] absolute top-[162px] -left-5 md:top-64 md:left-0'>
+          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff52] bg-opacity-[12%] absolute top-[162px] -left-5 md:top-64 md:left-0'>
             <Image src='/bracket_icon.svg' alt='bracket icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
-          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff] bg-opacity-[12%] absolute top-60 left-[149px] md:top-[570px] md:left-44'>
+          <div className='flex items-center justify-center rounded-full w-11 h-11 md:w-14 md:h-14 bg-[#f4ebff52] bg-opacity-[12%] absolute top-60 left-[149px] md:top-[570px] md:left-44'>
             <Image src='/github_icon.svg' alt='github icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
-          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff] md:bg-opacity-[12%] md:absolute  md:top-[592px] md:left-[608px]'>
+          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff52] md:bg-opacity-[12%] md:absolute  md:top-[592px] md:left-[608px]'>
             <Image src='/electricity_icon.svg' alt='electricity icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
-          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff] md:bg-opacity-[12%] md:absolute md:top-[158px] md:left-[606px]'>
+          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff52] md:bg-opacity-[12%] md:absolute md:top-[158px] md:left-[606px]'>
             <Image src='/merge_icon.svg' alt='merge icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
-          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff] md:bg-opacity-[12%] md:absolute md:top-[368px] md:left-[965px]'>
+          <div className='hidden md:flex md:items-center md:justify-center md:rounded-full sm:w-11 sm:h-11 md:w-14 md:h-14 md:bg-[#f4ebff52] md:bg-opacity-[12%] md:absolute md:top-[368px] md:left-[965px]'>
             <Image src='/stack_icon.svg' alt='stack icon' width={23} height={23} className='w-auto h-auto'/>
           </div>
         </div>
