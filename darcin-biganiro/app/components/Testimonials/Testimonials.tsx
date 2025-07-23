2384013aa355b2ab79f640585936d0e556a434d9
DarcinBig <biganirodarcin@gmail.com>
2025-07-23 17:02:51 +0200

    feat: implement Testimonials component with scrolling animation; add Testimonial subcomponent; update global styles and Tailwind config

--- darcin-biganiro/app/components/Testimonials/Testimonials.tsx
+++ darcin-biganiro/app/components/Testimonials/Testimonials.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React from 'react'
 import SectionContainer from '../Section/SectionContainer'
 import SectionHeader from '../Section/SectionHeader'
+import testimonials from '@/data/testimonials.json'
+import Testimonial from './Testimonial'
 
 const Testimonials = () => {
   return (
@@ -10,6 +14,21 @@ const Testimonials = () => {
           plainText='📢 Check out these'
           highlightText='Testimonials'
         />
+        <div className='w-full overflow-x-clip'>
+          <div className='w-[1758px] sm:w-[2754px] flex items-stretch justify-start gap-[23px] pr-[23px] overflow-x-clip animate-scroll'>
+            {
+              [...testimonials, ...testimonials].map((testimonial, id) => (
+                <Testimonial
+                  key={id}
+                  image={testimonial.image}
+                  name={testimonial.name}
+                  role={testimonial.role}
+                  description={testimonial.description}
+                />
+              ))
+            }
+          </div>
+        </div>
       </div>
     </SectionContainer>
   )
