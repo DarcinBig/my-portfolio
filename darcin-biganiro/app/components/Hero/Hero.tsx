3b4e9003e06b3f3d2b818ed24c868aa957d6f602
DarcinBig <biganirodarcin@gmail.com>
2025-07-23 03:54:44 +0200

    Update Hero component with revised hero message and description.

--- darcin-biganiro/app/components/Hero/Hero.tsx
+++ darcin-biganiro/app/components/Hero/Hero.tsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <section className='relative flex flex-row items-center z-20 mx-auto md:gap-[37px] md:mx-10'>
       <div className='flex flex-col gap-[13px] items-center md:gap-[34px] md:my-[58px] md:items-start'>
         <h1 className='text-2xl text-center font-semibold block md:text-[40px] md:text-start md:inline'>
-          <span className='-ml-3'>👋</span>Hi! I&apos;m Darcin, I&apos;m a {''} <span className='block text-[27px] highlight mt-2 md:text-[45px] md:inline md:mt-0'>Software Engineer and UI/UX Designer</span>
+          <span className='-ml-3'>👋</span>Hi! — Darcin here,<br /> <span className='block text-[27px] highlight mt-2 md:text-[45px] md:inline md:mt-0 '> a Software Engineer and UI/UX Designer</span>
         </h1>
         <p className='text-center text-sm mx-6 md:text-start md:mx-0'>
-          I&apos;m obsessed with code and helping startups create unique and helpful products.
+          I blend robust code with intuitive design to deliver growth-driven digital experiences for startups. Let&apos;s collaborate to turn your vision into standout, user-centric products.
         </p>
         <a href="mailto:d.biganiro28@gmail.com" className='self-center bg-primary text-white p-2.5 rounded flex gap-2.5 items-center text-sm md:self-start md:text-xl/6'>
-          Let&apos;s Connect{' '}
+          Get in touch{' '}
           <Image src='/arrow_right_icon.svg' width={23} height={23} alt='arrow right icon'/>
         </a>
       </div>
